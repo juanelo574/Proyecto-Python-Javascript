bdf3bc80a765ef64c0b9ed7aed046a8d0409821d
Yosoyfr <3216883330506@ingenieria.usac.edu.gt>
2025-11-20 22:37:56 -0600

    Ejemplo 7: Refactorización de codigo

--- static/js/components/product-card.js
+++ static/js/components/product-card.js
@@ -1,4 +1,13 @@
-console.log("Script cargado correctamente");
+import {
+    eliminarProducto as eliminarProductoAPI,
+    habilitarProducto as habilitarProductoAPI,
+    obtenerComentarios,
+    obtenerProductos,
+} from "../api.js";
+
+import { cargarProductos } from "../utils.js";
+
+import { mostrarMensaje } from "../ui.js";
 
 class ProductCard extends HTMLElement {
     constructor() {
@@ -73,8 +82,7 @@ class ProductCard extends HTMLElement {
 
         // Cargar comentarios
         const comentariosDiv = this.shadowRoot.getElementById("comentarios");
-        const res = await fetch(`/productos/${id}/comentarios`);
-        const comentarios = await res.json();
+        const comentarios = await obtenerComentarios(id);
         if (comentarios.length === 0) {
             comentariosDiv.innerHTML = "<em>No hay comentarios.</em>";
         } else {
@@ -97,115 +105,17 @@ class ProductCard extends HTMLElement {
 
 customElements.define("product-card", ProductCard);
 
-async function cargarProductos() {
-    const contenedor = document.getElementById("product-list");
-    contenedor.innerHTML = "";
-
-    const loader = document.getElementById("loader");
-    loader.style.display = "block";
-
-    // simular retardo de carga
-    await new Promise((resolve) => setTimeout(resolve, 3000));
-
-    try {
-        const response = await fetch("/productos");
-        if (!response.ok) throw new Error("Error al obtener los productos");
-
-        const productos = await response.json();
-        loader.style.display = "none";
-
-        if (productos.length === 0) {
-            contenedor.innerHTML = "<p>No hay productos disponibles.</p>";
-            return;
-        }
-
-        productos.forEach((producto) => {
-            const productCard = document.createElement("product-card");
-            productCard.setAttribute("nombre", producto.nombre);
-            productCard.setAttribute("precio", producto.precio);
-            productCard.setAttribute("habilitado", producto.habilitado);
-            productCard.setAttribute("id", producto.id);
-            productCard.setAttribute("imagen", producto.imagen || "");
-            contenedor.appendChild(productCard);
-        });
-    } catch (error) {
-        console.error("Error al cargar productos:", error);
-        loader.style.display = "none";
-        contenedor.innerHTML = "<p>Error al cargar los productos.</p>";
-    }
-}
-
 async function eliminarProducto(id) {
     if (!confirm("¿Está seguro de que desea eliminar este producto?")) {
         return;
     }
-    await fetch(`/productos/${id}`, {
-        method: "DELETE",
-    });
+    await eliminarProductoAPI(id);
     mostrarMensaje("Producto eliminado correctamente.");
     cargarProductos();
 }
 
 async function habilitarProducto(id) {
-    await fetch(`/productos/${id}/habilitar`, {
-        method: "GET",
-    });
+    await habilitarProductoAPI(id);
     mostrarMensaje("Producto habilitado/deshabilitado correctamente.");
     cargarProductos();
-}
-
-cargarProductos();
-
-const form = document.getElementById("form-product");
-const inputNombre = document.getElementById("nombre-producto");
-const inputPrecio = document.getElementById("precio-producto");
-const inputImagen = document.getElementById("imagen-producto");
-
-// Captura del formulario para agregar un nuevo producto
-form.addEventListener("submit", async (e) => {
-    e.preventDefault();
-
-    if (!inputNombre.value || !inputPrecio.value) {
-        alert("Por favor, complete todos los campos.");
-        return;
-    }
-
-    const formData = new FormData();
-    formData.append("nombre", inputNombre.value);
-    formData.append("precio", parseFloat(inputPrecio.value));
-    formData.append("imagen", inputImagen.files[0]);
-
-    const response = await fetch("/productos", {
-        method: "POST",
-        body: formData,
-    });
-
-    if (response.ok) {
-        inputNombre.value = "";
-        inputPrecio.value = "";
-        inputImagen.value = "";
-        mostrarMensaje("Producto agregado correctamente.");
-        cargarProductos();
-    } else {
-        alert("Error al agregar el producto.");
-    }
-});
-
-function mostrarMensaje(texto) {
-    const alerta = document.createElement("div");
-    alerta.textContent = texto;
-    alerta.style.position = "fixed";
-    alerta.style.top = "10px";
-    alerta.style.right = "10px";
-    alerta.style.zIndex = "1000";
-
-    alerta.style.padding = "10px";
-    alerta.style.margin = "10px 0";
-    alerta.style.borderRadius = "5px";
-    alerta.style.backgroundColor = "#e0ffe0";
-
-    document.body.appendChild(alerta);
-    setTimeout(() => {
-        alerta.remove();
-    }, 3000);
 }
